8295bed30baa2dfab34a16b1b9e3f6b1e8eda4dd
mubashirg22 <mubashirgolandaz98@gmail.com>
2026-06-23 01:07:25 +0530

    Refactor leave function and add employee agent calls

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
+
+EMPLOYEE_AGENT_CARD = \
+"https://employee-agent-fis1.onrender.com/.well-known/agent.json"
+
+EMPLOYEE_API = \
+"https://employee-agent-fis1.onrender.com/employee"
 
 @app.get("/")
 def home():
@@ -17,14 +24,24 @@ def agent_card():
         "version": "1.0",
         "skills": [
             {
-                "id": "leave_balance",
-                "description": "Get employee leave balance"
+                "id": "leave_approval",
+                "description": "Checks employee leave eligibility"
             }
         ]
     }
 
 @app.get("/leave/{empid}")
-def get_leave(empid: str):
+def leave_check(empid: str):
+
+    # Step 1: Discover Employee Agent
+    employee_card = requests.get(
+        EMPLOYEE_AGENT_CARD
+    ).json()
+
+    # Step 2: Invoke Employee Agent
+    employee = requests.get(
+        f"{EMPLOYEE_API}/{empid}"
+    ).json()
 
     leave_data = {
         "1001": 12,
@@ -32,8 +49,17 @@ def get_leave(empid: str):
         "1003": 20
     }
 
+    balance = leave_data.get(empid, 5)
+
+    decision = (
+        "Approved"
+        if balance >= 10
+        else "Rejected"
+    )
+
     return {
-        "employeeId": empid,
-        "leaveBalance": leave_data.get(empid, 5),
-        "status": "approved"
+        "agentDiscovery": employee_card["name"],
+        "employee": employee,
+        "leaveBalance": balance,
+        "decision": decision
     }
